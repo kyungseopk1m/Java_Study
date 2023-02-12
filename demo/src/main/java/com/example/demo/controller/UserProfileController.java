974770cd29569a77bb500953558a211bef70305e
kyungseopk1m <kxxseop@naver.com>
2023-02-12 16:58:00 +0900

    Oracle과 MyBatis 연동.  Spring Boot Starter MyBatis를 사용하였음.

--- demo/src/main/java/com/example/demo/controller/UserProfileController.java
+++ demo/src/main/java/com/example/demo/controller/UserProfileController.java
@@ -1,12 +1,6 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import javax.annotation.PostConstruct;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,19 +9,18 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.mapper.UserProfileMapper;
 import com.example.demo.model.UserProfile;
 
 @RestController
 public class UserProfileController {
 	
-	private Map<String, UserProfile> userMap;
+	private UserProfileMapper mapper;
 	
-	@PostConstruct
-	public void init() {
-		userMap = new HashMap<String, UserProfile>();
-		userMap.put("1", new UserProfile("1", "홍길동", "111-1111", "서울시 강남구 대치1동"));
-		userMap.put("2", new UserProfile("2", "홍길자", "111-1112", "서울시 강남구 대치2동"));
-		userMap.put("3", new UserProfile("3", "홍길순", "111-1113", "서울시 강남구 대치3동"));
+//	private Map<String, UserProfile> userMap;
+	
+	public UserProfileController(UserProfileMapper mapper) {
+		this.mapper = mapper;
 	}
 	
 	// 포트 8000 수정
@@ -36,31 +29,27 @@ public class UserProfileController {
 	
 	@GetMapping("/user/{id}") // 사용자 id를 기준으로 정보 조회
 	public UserProfile getUserProfile(@PathVariable("id") String id) {
-		return userMap.get(id);
+		return mapper.getUserProfile(id);
 	}
 	
 	@GetMapping("/user/all") // 전체 사용자 정보 조회
 	public List<UserProfile> getUserProfileList() {
-		return new ArrayList<UserProfile>(userMap.values());
+		return mapper.getUserProfileList();
 	}
 	
 	@PutMapping("/user/{id}") // id를 기준으로 사용자 정보 생성
 	public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
-		UserProfile userProfile = new UserProfile(id, name, phone, address);
-		userMap.put(id, userProfile);
+		mapper.insertUserProfile(id, name, phone, address);
 	}
 	
 	@PostMapping("/user/{id}") // id를 기준으로 사용자 정보 수정
 	public void postUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
-		UserProfile userProfile = userMap.get(id);
-		userProfile.setName(name);
-		userProfile.setPhone(phone);
-		userProfile.setAddress(address);
+		mapper.updateUserProfile(id, name, phone, address);
 	}
 	
 	@DeleteMapping("/user/{id}") // id를 기준으로 사용자 정보 삭제
 	public void deleteUserProfile(@PathVariable("id") String id) {
-		userMap.remove(id);
+		mapper.deleteUserProfile(id);
 	}
 	
 
